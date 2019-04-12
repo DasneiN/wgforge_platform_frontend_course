4b242b76fa3d9a92fbb50960dd3f79ecba761aff
DasneiN <sank.mail@gmail.com>
2019-04-12 21:29:39 +0300

    implement cloneDeep

--- 03_iterators/practice/exercises/cloneDeep.js
+++ 03_iterators/practice/exercises/cloneDeep.js
@@ -1,6 +1,16 @@
 /**
  * Реализовать функцию deepCopy которая копирует объекты по значению с любой глубиной вложенности
  */
-export default function cloneDeep() {
-  // ¯\_(ツ)_/¯
+export default function cloneDeep(obj) {
+  if (typeof obj === 'object' && !!obj) {
+    const result = {};
+
+    for (let key in obj) {
+      result[key] = cloneDeep(obj[key]);
+    }
+
+    return result;
+  }
+
+  return obj;
 }
